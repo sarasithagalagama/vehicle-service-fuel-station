a3061c316da1b6411b861cb07273e8341a82ffb0
IT24100548 <sarasithagalagama@gmail.com>
2025-08-10 11:19:40 +0530

    Initialize vehicle service fuel station project with Maven setup and Spring Boot configuration

---
+++ src/main/java/com/fuelstation/vehicle_service_fuel_station/VehicleServiceFuelStationApplication.java
@@ -0,0 +1,13 @@
+package com.fuelstation.vehicle_service_fuel_station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceFuelStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceFuelStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuelstation/vehicle_service_fuel_station/VehicleServiceFuelStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuelstation.vehicle_service_fuel_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceFuelStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
